c500c033ea5ad2a68cf6fdd7f62fc1ff9164d445
vandrade220-war <vandrade220@gmail.com>
2025-09-07 00:48:55 -0300

    07/09

--- testemerge.py
+++ testemerge.py
@@ -1,1 +1,22 @@
-print('lol')
+
+#fazer cadastro de aboilusno e notas colocar e uma lista e fazer a validação para cada aluno
+
+alunoss = []
+notass = []
+
+while True:
+   nome = input('qual nome do aluno')
+   if nome == '-1':
+       break
+   notas = input('qual a nota do aluno')
+   if notas == "-1":
+       break
+   alunoss.append(nome)
+   notass.append(notas)
+
+for i in range(len(alunoss)):
+    print(f'O aluno é: {alunoss[i]} e sua nota é {notass[i]}')
+
+
+
+
