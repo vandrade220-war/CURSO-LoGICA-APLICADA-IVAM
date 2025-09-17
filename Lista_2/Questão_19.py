56813f4eabf27ccf5a0289a8a4f88a20fda9b33f
vandrade220-war <vandrade220@gmail.com>
2025-09-17 16:15:25 -0300

    Update Questão_19.py

--- Lista_2/Questão_19.py
+++ Lista_2/Questão_19.py
@@ -5,16 +5,18 @@ while True:
 
     numero = int(input('Digite um número menor que 1000: '))
 
-    if numero > 1000:
-        int(input('Digite um número abaixo que seja abaixo de 1000 mazanza'))
-    else:
-        if numero < 1000:
-            centena = numero/100 
-            dezena = numero/10
-            break
+    if 0 <= numero < 1000:
+        centenas = numero // 100
+        dezenas = (numero % 100) // 10 
+        unidades = numero % 10 
 
-print(f'Essa é a quantidade de centenas: {centena}, essa é a quantidade de dezenas: {dezena}, essa é a quantidade de unidades {numero}')
-    
+        partes = []
+        if centenas > 0:
+            partes.append(f'{centenas} centena' + ("s" if centenas > 1 else''))
+        if dezenas > 0:
+             partes.append(f'{dezenas} dezenas' + ("s" if dezenas > 1 else''))
+        if unidades > 0:
+             partes.append(f'{unidades} unidades' + ("s" if unidades > 1 else''))
 
-
-    
+    print(f"{numero} = " + ", ".join(partes[:-1]) + (" e " if len(partes) > 1 else "") + partes[-1])
+         
